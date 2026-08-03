afa06d6955665e1970961f855b86d798ae1b239e
ali.sdet <khabibullaev.19@gmail.com>
2026-08-03 23:02:46 +0500

    unary oper

---
+++ projectsStartedJul_01_2026/javacopies/src/pack04operators/UnaryOperators.java
@@ -0,0 +1,15 @@
+package pack04operators;
+
+public class UnaryOperators {
+
+    /*
+
+        Unary Operators:
+        1. " + " => Unary Plus Operator -> Indicates positive value.
+        2. " - " => Unary Minus Operator -> Negates an expression or value.
+        3. " ++ " => Increment Operator -> Increments a value by 1.
+        4. " -- " => Decrement Operator -> Decrements a value by 1.
+
+     */
+
+}
