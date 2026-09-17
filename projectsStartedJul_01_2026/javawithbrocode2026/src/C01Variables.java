66a4b7384a8d85c37d18c076a80ebbe107810095
ali.sdet <khabibullaev.19@gmail.com>
2026-09-18 03:36:52 +0500

    bro code start on sep18

--- projectsStartedJul_01_2026/javawithbrocode2026/src/C01Variables.java
+++ projectsStartedJul_01_2026/javawithbrocode2026/src/C01Variables.java
@@ -1,4 +1,67 @@
-package PACKAGE_NAME;
+public class C01Variables {
 
-public class c01Variables {
+    /*
+
+        Variable = a reusable container for a value.
+                   a variable behaves as if it was the value it contains.
+
+        Primitive = simple value stored directly in memory(stack)
+        Reference = memory address (stack) that points to the (heap)
+
+        📌Primitive   vs   📍Reference
+            int              String
+            double           Array
+            char             Object
+            boolean
+
+            2 Steps to create a variable.
+            1. declaration
+            2. assignment
+     */
+
+    public static void main(String[] args) {
+
+        int age = 24;
+        int year = 2026;
+        int quantity = 1;
+
+        double price = 19.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = false;
+        boolean isTester = true;
+        boolean isOnline = true;
+
+        String name = "Ali";
+        String food = "Pizza";
+        String email = "fake123@gmail.com";
+        String car = "BMW";
+        String color = "red";
+
+        System.out.println(food);
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(car);
+        System.out.println(color);
+
+        System.out.println();
+
+        System.out.println(age);
+        System.out.println("The year is " + year);
+        System.out.println(temperature);
+        System.out.println("The currency is " + currency);
+
+        if (isTester == true) {
+            System.out.println("He is a tester? " + isTester);
+        }
+
+        System.out.println("Hello " + name);
+
+    }
+
 }
--- projectsStartedJul_01_2026/javawithbrocode2026/src/FirstClass.java
+++ projectsStartedJul_01_2026/javawithbrocode2026/src/FirstClass.java
@@ -1,2 +1,13 @@
 public class FirstClass {
+
+    public static void main(String[] args){
+
+        System.out.print("Hello World1\n");
+        System.out.print("Hello World2\n");
+        System.out.print("Hello World3\n");
+        System.out.println("Hello World4");
+        System.out.print("Hello World5\n");
+
+    }
+
 }
